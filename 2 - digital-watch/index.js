6843b85f4ec464da77faed49f2491f5eb2b091f5
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-10-22 00:05:27 +0700

    fix: hour get and times format

--- 2 - digital-watch/index.js
+++ 2 - digital-watch/index.js
@@ -10,14 +10,14 @@ function updateClock() {
   // Форматирование времени под AM PM
   let hour = date.getHours();
   hour = hour % 12;
-  hour = hour ? hour : 12;
+  hour = hour ? hour : date.getHours();
 
   // Изменение DOM элементов
   day.innerHTML = daysWeek[date.getDay()];
   hours.innerHTML = hour < 10 ? "0" + hour : hour;
   minutes.innerHTML =
     date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-  timeDays.innerHTML = hours >= 12 ? "PM" : "AM";
+  timeDays.innerHTML = hour >= 12 ? "PM" : "AM";
 }
 
 updateClock(); // Вызываем для получения даты
