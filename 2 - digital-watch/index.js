32301fcebe16793e1cef48e897d34ec2c80b3913
Nikita Pozhidaev <pozhidaev2002@inbox.ru>
2022-10-21 23:09:02 +0700

    add: digital watch

---
+++ 2 - digital-watch/index.js
@@ -0,0 +1,25 @@
+const day = document.querySelector(".day");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const timeDays = document.querySelector(".pm-am");
+const daysWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function updateClock() {
+  const date = new Date();
+
+  // Форматирование времени под AM PM
+  let hour = date.getHours();
+  hour = hour % 12;
+  hour = hour ? hour : 12;
+
+  // Изменение DOM элементов
+  day.innerHTML = daysWeek[date.getDay()];
+  hours.innerHTML = hour < 10 ? "0" + hour : hour;
+  minutes.innerHTML =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  timeDays.innerHTML = hours >= 12 ? "PM" : "AM";
+}
+
+updateClock(); // Вызываем для получения даты
+
+setInterval(updateClock, 10000); // Каждые 10 секунд получаем время и обновляем DOM элементы
